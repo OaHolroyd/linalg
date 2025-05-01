c0033005c5c729c276488fb29a8f1980f1bb842f
oscar <oaholroyd@gmail.com>
2025-05-01 17:47:01 +0100

    check for correct formatting

--- src/tests/testing.h
+++ src/tests/testing.h
@@ -1,48 +1,58 @@
 #ifndef TESTING_H
 #define TESTING_H
 
-#include <stdio.h>
 #include <math.h>
-
+#include <stdio.h>
 
 const char *TESTN;
 const char *SUBTESTN;
 int ERR_COUNT;
 
-#define START_TEST(title) { TESTN = title; SUBTESTN = ""; ERR_COUNT = 0; }
-
-
-#define SUBTEST(subtitle) {                      \
-    SUBTESTN = subtitle;                       \
-    fprintf(stderr, "  %s:%s\n", TESTN, SUBTESTN); \
+#define START_TEST(title)                                                      \
+  {                                                                            \
+    TESTN = title;                                                             \
+    SUBTESTN = "";                                                             \
+    ERR_COUNT = 0;                                                             \
   }
 
+#define SUBTEST(subtitle)                                                      \
+  {                                                                            \
+    SUBTESTN = subtitle;                                                       \
+    fprintf(stderr, "  %s:%s\n", TESTN, SUBTESTN);                             \
+  }
 
-#define REQUIRE(cond) { int l = __LINE__;          \
-    if (!(cond)) {                                 \
-      ERR_COUNT++;                                 \
-      fprintf(stderr, "    line %d: FAILED\n", l); \
-    }                                              \
+#define REQUIRE(cond)                                                          \
+  {                                                                            \
+    int l = __LINE__;                                                          \
+    if (!(cond)) {                                                             \
+      ERR_COUNT++;                                                             \
+      fprintf(stderr, "    line %d: FAILED\n", l);                             \
+    }                                                                          \
   }
 
 // as above but exits early if the condition is not met
-#define REQUIRE_BARRIER(cond) { int l = __LINE__;   \
-    if (!(cond)) {                                  \
-      ERR_COUNT++;                                  \
-      fprintf(stderr, "    line %d: ABORTED\n", l); \
-      return 1;                                     \
-    }                                               \
+#define REQUIRE_BARRIER(cond)                                                  \
+  {                                                                            \
+    int l = __LINE__;                                                          \
+    if (!(cond)) {                                                             \
+      ERR_COUNT++;                                                             \
+      fprintf(stderr, "    line %d: ABORTED\n", l);                            \
+      return 1;                                                                \
+    }                                                                          \
   }
 
-#define REQUIRE_CLOSE(x, y, tol) { int l = __LINE__; \
-    if (fabs(((double)(x)) - ((double)(y))) > tol) { \
-      ERR_COUNT++;                                   \
-      fprintf(stderr, "    line %d: FAILED\n", l);   \
-    }                                                \
+#define REQUIRE_CLOSE(x, y, tol)                                               \
+  {                                                                            \
+    int l = __LINE__;                                                          \
+    if (fabs(((double)(x)) - ((double)(y))) > tol) {                           \
+      ERR_COUNT++;                                                             \
+      fprintf(stderr, "    line %d: FAILED\n", l);                             \
+    }                                                                          \
   }
 
+#define END_TEST()                                                             \
+  {                                                                            \
+    return ERR_COUNT;                                                          \
+  }
 
-#define END_TEST() { return ERR_COUNT; }
-
-
-#endif //TESTING_H
+#endif // TESTING_H
